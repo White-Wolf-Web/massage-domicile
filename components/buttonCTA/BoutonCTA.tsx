344cd9fd86958b7cc910423eb5f52ba6b18af963
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2023-11-18 13:55:30 +0100

    Update BoutonCTA.tsx

--- components/buttonCTA/BoutonCTA.tsx
+++ components/buttonCTA/BoutonCTA.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 export function BoutonCTA  () {
   return (
     <div className={styles.boutonCTAContainer}>
-      <Link href="/contact" className={styles.boutonCTA}>
+      <p className={styles.boutonCTA}>
           Contactez-nous  <br />    0477131993
-      </Link>
+      </p>
     </div>
   );
 };
